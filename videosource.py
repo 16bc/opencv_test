421acf7e4e6f593d22272a440b2f1fcddd41da28
16bc <github@16bc.ru>
2022-08-24 22:57:04 +0500

    Устранён рассинхрон

--- videosource.py
+++ videosource.py
@@ -7,13 +7,16 @@ from threading import Thread
 class VideoSource:
     def __init__(self, video_url: str):
         self.fps = 30
+        self.period = 1/self.fps
         self.output_frame = None
         self.cap = None
         self.traficLight_value = ''
         self.status = False
         self.frame = None
         while True:
+            # self.cap = cv2.VideoCapture(video_url, cv2.CAP_FFMPEG, [cv2.CAP_PROP_HW_ACCELERATION, cv2.VIDEO_ACCELERATION_ANY ])
             self.cap = cv2.VideoCapture(video_url)
+            self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 2)
             if not self.cap.isOpened():
                 print('!!! Unable to open URL')
                 time.sleep(5)
@@ -26,25 +29,30 @@ class VideoSource:
         self.process.start()
 
     def _process_frame(self):
+        every_second_frame = True  # Флаг каждого второго кадра для обработки
         while True:
+            start_time = time.time()
             if self.cap.isOpened():
-                # self.status, self.frame = self.cap.read()
                 _status, _raw_frame = self.cap.read()
                 if not _status:
                     continue
-                # Определяем цвет светофора
-                # self.output_frame = detect_light_state(frame)
                 _raw_frame = cv2.resize(_raw_frame, (1280, 800))
-                traficLight_frame = _raw_frame[521:531, 634:644]
-                traficLight = cv2.resize(traficLight_frame, (1, 1))
-                self.traficLight_value = 'Красный' if traficLight[0, 0][2] > 150 else 'Зеленый'
+
+                # Определяем цвет светофора каждый второй кадр для снижения нагрузки
+                if every_second_frame:
+                    traficLight_frame = _raw_frame[521:531, 634:644]
+                    traficLight = cv2.resize(traficLight_frame, (1, 1))
+                    self.traficLight_value = 'Красный' if traficLight[0, 0][2] > 150 else 'Зеленый'
                 cv2.rectangle(_raw_frame, (90, 40), (420, 120),
-                              (255, 255, 255), 3)
+                              (0,0,0), -1)
                 cv2.putText(_raw_frame, self.traficLight_value, (100, 100), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255))
                 self.frame = _raw_frame
                 self.status = _status
                 self._encode_image()
-            time.sleep(1/self.fps)
+                every_second_frame = not every_second_frame
+                dt = time.time() - start_time
+                if self.period - dt > 0:
+                    time.sleep(self.period - dt)
 
     def _encode_image(self):
         _, encodedImage = cv2.imencode(".jpg", self.frame)
